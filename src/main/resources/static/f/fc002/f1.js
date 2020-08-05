eef80f4619209e17eb1f0fafef92d3e744a76531
romanm <roman.mishchenko@gmail.com>
2020-08-05 19:22:01 +0300

    prepare to x

--- src/main/resources/static/f/fc002/f1.js
+++ src/main/resources/static/f/fc002/f1.js
@@ -7,15 +7,16 @@ app.controller('AppCtrl', function ($scope, $http, $timeout) {
     read_element_descendant(368815, function (response) {
         read_element_descendant(371903, function (response) {
             $timeout(function () {
-                createRightJSON({}, ctrl.eMap[371903])
+                createRightJSON({}, ctrl.eMap[371903], $timeout)
             }, 400);
         })
     })
 })
 
-var createRightJSON = function (rJ, data) {
+var createRightJSON = function (rJ, data, $timeout) {
     var key = extractKey(data)
     rJ[key] = data.value_1_22
+    rJ.doc_id = data.doc_id
     console.log(rJ, data)
     angular.forEach(data.children, function (d) {
         var k2 = extractKey(d)
@@ -26,28 +27,52 @@ var createRightJSON = function (rJ, data) {
         }
         // createRightJSON(rJ, d)
     })
+    $timeout(function () {
+        ctrl.pfNiceStr = JSON.stringify({ 'PlanDefinition': rJ }, undefined, 2)
+        // console.log('PlanDefinition:', rJ, data, ctrl.pfNiceStr)
+    }, 200)
 }
 
 function addListRJ(rJ, data, key) {
     angular.forEach(data.children, function (d) {
         var e = { doc_id: d.doc_id }
         rJ[key].push(e)
-        console.log(d.children, d.doc_id)
         angular.forEach(d.children, function (d2) {
             var k2 = extractKey(d2)
-            console.log(d2.doc_id, k2)
+            if (!k2) {
+                read_element(d2.reference, function () {
+                    var k3 = extractKey(d2)
+                    if (ctrl.eMap[d2.reference].parent == d2.reference2) {
+                        var ne = e[d2.r2value] = {}
+                        ne[k3] = d2.value_1_22
+                        ne.doc_id = d2.doc_id
+                        // console.log( 'PlanDefinition:', rJ)
+                        if('ActivityDefinition' == d2.r2value){
+                            console.log(111)
+                            read2.sql1({fn:function(response){
+                                var parent = response.data.list[0].parent
+                                console.log(parent)
+                            },params:{
+                                sql:"SELECT * FROM doc where reference=371927 and reference2 = " + d2.doc_id
+                            }})
+                        }
+                    }
+                })
+            }
             // createRightJSON(e, d2)
         })
     })
-    console.log(rJ, data, key)
 }
 
 function extractKey(data) {
     var key = data.r1value
     if (!key) {
-        key = ctrl.eMap[data.reference].value_1_22
-        if (!key) {
-            var key = ctrl.eMap[data.reference].r1value
+        var eDataRef = ctrl.eMap[data.reference]
+        if (eDataRef) {
+            key = eDataRef.value_1_22
+            if (!key) {
+                var key = eDataRef.r1value
+            }
         }
     }
     return key
